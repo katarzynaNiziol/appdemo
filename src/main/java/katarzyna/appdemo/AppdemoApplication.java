ce7f2684709a5dbaf34e72dabe15fa8c816f79b6
Katarzyna <katarzyna.niziol1993@gmail.com>
2022-10-22 20:46:31 +0200

    Initial commit

---
+++ src/main/java/katarzyna/appdemo/AppdemoApplication.java
@@ -0,0 +1,15 @@
+package katarzyna.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class AppdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppdemoApplication.class, args);
+    }
+
+}
